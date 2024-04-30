e8415ba584581e535d697ced1c9f02521b66d05a
Nathan Brian Nordstrom-Hearne <119480325+NathanN-H@users.noreply.github.com>
2024-04-30 14:33:53 +0100

    Update home.php

--- home.php
+++ home.php
@@ -15,7 +15,6 @@
         <meta charset="UTF-8">
         <title>Lost Pets</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins">
         <link rel="shortcut icon" type="image/x-icon" href="Design-Images/cat_shortcuticon.png">
         <style>
